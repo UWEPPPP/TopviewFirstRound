68b930cc356500680fc86f4bc72cec95a1763624
liu <a2506991786@qq.com>
2023-03-15 23:06:49 +0800

    初始化

---
+++ test/com/liujiahui/www/TestTest.java
@@ -0,0 +1,10 @@
+package com.liujiahui.www;
+
+import junit.framework.TestCase;
+
+public class TestTest extends TestCase {
+
+    public void testTestMain() {
+        new Test().testMain();
+    }
+}
